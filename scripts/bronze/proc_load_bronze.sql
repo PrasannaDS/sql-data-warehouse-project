e7b8c9716a360b17aa2c9902bcc85e2654b214f6
Prasanna Lakshmi <python.prasanna@gmail.com>
2026-05-09 16:09:51 +0530

    Improve print statements for loading process

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -21,17 +21,22 @@ BEGIN
     DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @batch_end_time DATETIME;
     BEGIN TRY
     SET @batch_start_time = GETDATE();
-        PRINT '==============================================';
-        PRINT 'Loading Bronze layer';
-        PRINT '==============================================';
+        PRINT '                   ==============================================';
+        PRINT '                                Loading Bronze layer             ';
+        PRINT '                   ==============================================';
 
-        PRINT '----------------------------------------------';
-        PRINT 'Loading CRM Tables';
-        PRINT '----------------------------------------------';
+        PRINT '                   ----------------------------------------------';
+        PRINT '                                 Loading CRM Tables              ';
+        PRINT '                   ----------------------------------------------';
         
         SET @start_time = GETDATE();
         PRINT '>> Truncating Table: bronze.crm_cust_info';
         TRUNCATE TABLE bronze.crm_cust_info;
+
+        PRINT '
+        --> Loading data from CSV file as is without any changes.
+        --> Specifying the FIRSTROW and FIELDTERMINATOR values while performing BULK INSERT.
+        ';
 
         PRINT '>> Inserting data into Table: bronze.crm_cust_info';
         BULK INSERT bronze.crm_cust_info
@@ -43,27 +48,37 @@ BEGIN
         );
         SET @end_time = GETDATE();
         PRINT '>> Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR(20)) + ' seconds';
-        PRINT '-----------------------';
+        PRINT '==================================================================================';
 
         SET @start_time = GETDATE();
         PRINT '>> Truncating Table: bronze.crm_prd_info';
         TRUNCATE TABLE bronze.crm_prd_info;
+        
+        PRINT '
+        --> Loading data from CSV file as is without any changes.
+        --> Specifying the FIRSTROW and FIELDTERMINATOR values while performing BULK INSERT.
+        ';
 
         PRINT '>> Inserting data into Table: bronze.crm_prd_info';
         BULK INSERT bronze.crm_prd_info
         FROM '/var/opt/mssql/prd_info.csv'
         WITH (
-        FIRSTROW = 2,
-        FIELDTERMINATOR = ',',
-        TABLOCK
+            FIRSTROW = 2,
+            FIELDTERMINATOR = ',',
+            TABLOCK
         );
         SET @end_time = GETDATE();
         PRINT '>> Load Duration:' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR(20)) + ' seconds'; 
-        PRINT '-----------------------';
+        PRINT '==================================================================================';
 
         SET @start_time = GETDATE();
         PRINT '>> Truncating Table: bronze.crm_sales_details';
         TRUNCATE TABLE bronze.crm_sales_details;
+
+        PRINT '
+        --> Loading data from CSV file as is without any changes.
+        --> Specifying the FIRSTROW and FIELDTERMINATOR values while performing BULK INSERT.
+        ';
 
         PRINT '>> Inserting data into Table: bronze.crm_sales_details';
         BULK INSERT bronze.crm_sales_details
@@ -75,15 +90,20 @@ BEGIN
         );
         SET @end_time = GETDATE();
         PRINT '>> Load Duration:' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR(20)) + ' seconds';
-        PRINT '-----------------------';
+        PRINT '==================================================================================';
 
-        PRINT '----------------------------------------------';
-        PRINT 'Loading ERP Tables';
-        PRINT '----------------------------------------------';
+        PRINT '                   ----------------------------------------------';
+        PRINT '                                 Loading ERP Tables              ';
+        PRINT '                   ----------------------------------------------';
 
         SET @start_time = GETDATE();
         PRINT '>> Truncating Table: bronze.erp_cust_az12';
         TRUNCATE TABLE bronze.erp_cust_az12;
+
+        PRINT '
+        --> Loading data from CSV file as is without any changes.
+        --> Specifying the FIRSTROW and FIELDTERMINATOR values while performing BULK INSERT.
+        ';
 
         PRINT '>> Inserting data into Table: bronze.erp_cust_az12';
         BULK INSERT bronze.erp_cust_az12
@@ -95,11 +115,16 @@ BEGIN
         );
         SET @end_time = GETDATE();
         PRINT '>> Load Duration:' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR(20)) + ' seconds'; 
-        PRINT '-----------------------';
+        PRINT '==================================================================================';
 
         SET @start_time = GETDATE();
         PRINT '>> Truncating Table: bronze.erp_loc_a101';
         TRUNCATE TABLE bronze.erp_loc_a101;
+
+        PRINT '
+        --> Loading data from CSV file as is without any changes.
+        --> Specifying the FIRSTROW and FIELDTERMINATOR values while performing BULK INSERT.
+        ';
         
         PRINT '>> Inserting data into Table: bronze.erp_loc_a101';
         BULK INSERT bronze.erp_loc_a101
@@ -111,11 +136,16 @@ BEGIN
         );
         SET @end_time = GETDATE();
         PRINT '>> Load Duration:' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR(20)) + ' seconds'; 
-        PRINT '-----------------------';
+        PRINT '==================================================================================';
 
         SET @start_time = GETDATE();
         PRINT '>> Truncating Table: bronze.erp_px_cat_g1v2';
         TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+
+        PRINT '
+        --> Loading data from CSV file as is without any changes.
+        --> Specifying the FIRSTROW and FIELDTERMINATOR values while performing BULK INSERT.
+        ';
 
         PRINT '>> Inserting data into Table: bronze.erp_px_cat_g1v2';
         BULK INSERT bronze.erp_px_cat_g1v2
@@ -127,21 +157,21 @@ BEGIN
         );
         SET @end_time = GETDATE();
         PRINT '>> Load Duration:' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR(20)) + ' seconds';
-        PRINT '-----------------------';
+        PRINT '==================================================================================';
     
         SET @batch_end_time = GETDATE();
-        PRINT '==============================================';
-        PRINT 'Loading Bronze layer completed';
-        PRINT '   - Total Load Duration: ' + CAST(DATEDIFF(SECOND, @batch_start_time, @batch_end_time) AS NVARCHAR(20)) + ' seconds';
-        PRINT '==============================================';
+        PRINT '==================================================================================';
+        PRINT '            LOADING DATA INTO BRONZE LAYER IS COMPLETED!                          ';
+        PRINT '           - Total Load Duration: ' + CAST(DATEDIFF(SECOND, @batch_start_time, @batch_end_time) AS NVARCHAR(20)) + ' seconds';
+        PRINT '==================================================================================';
     END TRY
     BEGIN CATCH
-        PRINT'===========================================================================';   
+        PRINT'====================================================================================';   
         PRINT 'ERROR OCCURED WHILE LOADING BRONZE LAYER!'; 
         PRINT 'Error Message: ' + ERROR_MESSAGE();    
         PRINT 'Error Number: ' + CAST(ERROR_NUMBER() AS NVARCHAR(20));
         PRINT 'Error State: ' + CAST(ERROR_STATE() AS NVARCHAR(20));
         PRINT 'Error Line: ' + CAST(ERROR_LINE() AS NVARCHAR(20));
-        PRINT'===========================================================================';        
+        PRINT'====================================================================================';        
     END CATCH
     END;
