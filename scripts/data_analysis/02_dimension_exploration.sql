5a469e40c5897239d8fb67188d3a10326a78738a
Prasanna Lakshmi <python.prasanna@gmail.com>
2026-06-08 21:40:08 +0530

    Enhance documentation in dimension exploration SQL
    Added comments and documentation for dimension exploration.

--- scripts/data_analysis/02_dimension_exploration.sql
+++ scripts/data_analysis/02_dimension_exploration.sql
@@ -1,6 +1,16 @@
--- Dimension Exploration
--- Explore All Countries our customers come from.
+/*
+=======================================================================
+Dimension Exploration
+=======================================================================
+Identifying the unique values (or categories) in each dimension.
+Reorganizing how data might be grouped or segmented, which is useful
+for layer analysis ==> DISTINCT [Dimension]
+  - Explore All Countries that our customers come from.
+  - Explore All Categories "The major Divisions" 
+=======================================================================
+*/
 
+-- Explore All Countries that our customers come from
 SELECT DISTINCT country 
 FROM gold.dim_customers
 
