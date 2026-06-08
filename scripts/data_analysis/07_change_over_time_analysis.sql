4a905ff57ea65edc0ad0e14ae47cfabfd003623a
Prasanna Lakshmi <python.prasanna@gmail.com>
2026-06-08 22:19:57 +0530

    Enhance documentation for change-over-time analysis SQL
    Updated comments and added documentation for change-over-time analysis.

--- scripts/data_analysis/07_change_over_time_analysis.sql
+++ scripts/data_analysis/07_change_over_time_analysis.sql
@@ -1,10 +1,19 @@
--- "Answers Business Questions"
--- Using Complex Queries, Window Functions, CTE, Subquery, and Reports
+/*
+==============================================================================================
+Change-Over-Time Analysis
+==============================================================================================
+"Answers Business Questions" : Using Complex Queries, Window Functions, CTE, Subquery, 
+and Reports. Analyze how a measure evolves over the time. Helps track the trends and identify 
+seasonality in the data.
 
--- Change-Over-Time Trends --> Measure By Date Dimension
+Syntax ==> SUM/COUNT/AVG[MEASURE] By [DATE DIMENSION]
 
+  - Analyze Sales Performance Over Time(Month By Month)
+  - Analyze Sales Performance Over Time(Year By Year)
+  - Analyze Sales Performance Over Time(Different Date format)
+==============================================================================================
+*/
 -- Analyze Sales Performance Over Time
-
 SELECT
 DATETRUNC(month, order_date) AS order_month,
 SUM(sales_amount) AS total_sales,
@@ -27,7 +36,6 @@ GROUP BY DATETRUNC(year, order_date)
 ORDER BY DATETRUNC(year, order_date);
 
 -- For different format
-
 SELECT
 FORMAT(order_date, 'yyyy-MMM') AS order_year,
 SUM(sales_amount) AS total_sales,
