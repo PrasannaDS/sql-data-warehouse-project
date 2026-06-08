1885f41bd5364a55b491af4b17a766700ae6b5f1
Prasanna Lakshmi <python.prasanna@gmail.com>
2026-06-08 21:35:02 +0530

    Enhance SQL script with documentation comments
    Added comments and documentation for clarity on database exploration queries.

--- scripts/data_analysis/01_database_exploration.sql
+++ scripts/data_analysis/01_database_exploration.sql
@@ -1,8 +1,15 @@
--- Database Exploration
-
--- Explore All Objects in the database
+/*
+=======================================================================
+Database Exploration
+=======================================================================
+Getting database schema with their specifications and objects
+  - Explore All Tables or Objects in the database
+  - Explore All or Specific Columns in the Database 
+=======================================================================
+*/
+-- Explore All Tables or Objects in the database
 SELECT * FROM INFORMATION_SCHEMA.TABLES
 
--- Explore All Columns in the Database 
+-- Explore All or Specific Columns in the Database 
 SELECT * FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_NAME = 'dim_customers'
