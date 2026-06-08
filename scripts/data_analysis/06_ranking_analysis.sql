da314c1bdb80de62fc5e469ba45e1fca7f4e20f3
Prasanna Lakshmi <python.prasanna@gmail.com>
2026-06-08 22:14:52 +0530

    Update 06_ranking_analysis.sql

--- scripts/data_analysis/06_ranking_analysis.sql
+++ scripts/data_analysis/06_ranking_analysis.sql
@@ -1,6 +1,24 @@
+/*
+==============================================================================================
+Ranking Analysis
+==============================================================================================
+Also called Top N - Bottom N Analysis;
+    - Order the values of dimensions by measure.
+    - Top N Performers | Bottom N Performers
+    
+Syntax ==> RANK[DIMENSION] By SUM/COUNT/AVG[MEASURE]
+           Functions used - RANK(), DENSE_RANK(), ROW_NUMBER()
 
--- Ranking Analysis
-
+  - Which 5 products generate the highest revenue without using window function? 
+  - Which 5 products generate the highest revenue using window function? 
+  - Find the top 5 subcategories that generate highest revenue.
+  - What are the 5 worst-performing products in-terms of sales without using window function?
+  - What are the 5 worst-performing products in-terms of sales using window function?
+  - Find the top 5 subcategories that shows worst-performance.
+  - Find the top 10 customers who have generated the highest revenue
+  - Find the 3 customers with fewest orders placed
+==============================================================================================
+*/
 -- Which 5 products generate the highest revenue?
 
 SELECT TOP 5
